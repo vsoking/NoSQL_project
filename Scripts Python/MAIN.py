b99e42af27e2db07432ce9e078b0c71a7e654ada
AlannG <80313928+Dybalann@users.noreply.github.com>
2022-02-05 17:07:05 +0100

    Update Script 4.0

--- Scripts Python/MAIN.py
+++ Scripts Python/MAIN.py
@@ -1,22 +1,24 @@
 # ------------------------------------------------------------------------
 # Created by - Alann Goerke
-# Version - 2.2
-# Last Update - 02.03.2022
+# Version - 4.0
+# Last Update - 02.05.2022
 
 
 # ------------------------------------------------------------------------
 # --- IMPORTS
 # ------------------------------------------------------------------------
-import pandas as pd
-import time as t
-import subprocess
 import sys
-import os
+import time as t
+import pandas as pd
+
+#from cassandra.cluster import Cluster
+
 
 # ------------------------------------------------------------------------
 # --- GLOBAL VARIABLES
 # ------------------------------------------------------------------------
 path = '/Users/alann/PythonProjects/INF728-NoSQL/'
+# path = '/home/ubuntu/'
 
 col_events_name = [
     # --- Dates
@@ -57,8 +59,8 @@ col_gkg_name = [''] # --- To be completed
 # --- FUNCTIONS
 # ------------------------------------------------------------------------
 def generate_zip_files(initial_date, final_date):
-    '''Generates all zip files to download between a user defined period
-    of time.
+    '''Generates all url zip-files, for each date between a user defined 
+    period of time.    
 
     Parameters
     ---------
@@ -67,9 +69,12 @@ def generate_zip_files(initial_date, final_date):
 
     Return
     -----
-    - df: type: DataFrame
-        All the zip files for English and Translingual : export, mentions
-        and gkg
+    - dict_zip_files: type: dict
+        For each date, it contains 6 url where the data can be imported
+        into a dataframe, or dowloaded with a wget cmd.
+        . English : eng-export, eng-mentions, eng-gkg
+        . Translingual : translingual-export, translingual-mentions, 
+        translingual-gkg
     
     '''
     # --- Local variables
@@ -88,11 +93,15 @@ def generate_zip_files(initial_date, final_date):
 
     # --- Columns insertion : export, mentions, gkg  
     for csv in file_type:
-        df.insert(loc=df.shape[1],
+        # --- English files
+        df.insert(
+            loc=df.shape[1],
             column='eng-'+csv.split('.')[1],
             value=df['zip'].apply(lambda x: x+csv))
-    
-        df.insert(loc=df.shape[1],
+
+        # --- Translingual files
+        df.insert(
+            loc=df.shape[1],
             column='translingual-'+csv.split('.')[1],
             value=df['zip'].apply(lambda x: x+'.translation'+csv))
 
@@ -100,50 +109,31 @@ def generate_zip_files(initial_date, final_date):
     del df['date-str']
     del df['zip']
 
-    return df
+    # --- Store all the csv url files in a dictionnary
+    dict_zip_files = {}
+
+    for date in df.index:
+        dict_zip_files[str(date)] = {
+            'eng-export': df.loc[date]['eng-export'],
+            'eng-mentions': df.loc[date]['eng-mentions'],
+            'eng-gkg': df.loc[date]['eng-gkg'],
+            'translingual-export': df.loc[date]['translingual-export'],
+            'translingual-mentions': df.loc[date]['translingual-mentions'],
+            'translingual-gkg': df.loc[date]['translingual-gkg']
+        }
+
+    return dict_zip_files
 
 
 # ------------------------------------------------------------------------
-def local_cmd(cmd, visible_output=False):
-    '''Run a cmd bash command.
-
-    Parameters
-    ---------
-    - cmd: type: list
-        The terminal command we want to execute
-    - visible_output: type: bool
-        If we want to see the output of the cmd execution: out, err, ex
-
-    '''
-    # --- Run the wget bash command
-    proc = subprocess.Popen(cmd,
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE)
-    
-    try:
-        out, err = proc.communicate(timeout=10)
-        code = proc.returncode
-        if visible_output:
-            print("OUT: '{}'".format(out))
-            print("ERR: '{}'".format(err))
-            print("EXIT: {}".format(code))
-    except subprocess.TimeoutExpired:
-        proc.kill()
-        print("TIMEOUT")
-
-
-# ------------------------------------------------------------------------
-def read_zip_files(dict_files, folder_path):
-    '''Read some of the zip files downloaded with Pandas and concatenate
+def read_zip_files(dict_files):
+    '''Read zip files downloaded with Pandas and concatenate
     the English and Translingual files.
 
     Parameters
     ---------
     - dict_files: type: dict
-        Containing all the zip name files for a given date
-    - folder_path: type: str
-        Folder where the zip files are uploaded
+        Contains all the url zip name files for a given date
 
     Return
     -----
@@ -151,35 +141,38 @@ def read_zip_files(dict_files, folder_path):
         Dictionnary with all DataFrames inside for each zip file
 
     '''
+    # --------------------------------------------------------------------
     # --- Eng articles
     df_export = pd.read_csv(
-        folder_path+dict_files['eng-export'].split('/')[-1], 
+        dict_files['eng-export'], 
         sep='\t',
         names=col_events_name,
         header=None)
 
     df_mentions = pd.read_csv(
-        folder_path+dict_files['eng-mentions'].split('/')[-1], 
+        dict_files['eng-mentions'], 
         sep='\t',
         names=col_mentions_name,
         header=None)
     
     # --- Other countries articles
     df_export_translingual = pd.read_csv(
-        folder_path+dict_files['translingual-export'].split('/')[-1], 
+        dict_files['translingual-export'], 
         sep='\t',
         names=col_events_name,
         header=None)
 
     df_mentions_translingual = pd.read_csv(
-        folder_path+dict_files['translingual-mentions'].split('/')[-1], 
+        dict_files['translingual-mentions'], 
         sep='\t',
         names=col_mentions_name,
         header=None)
-    
+
+    # --------------------------------------------------------------------
     # --- Concatenate DataFrames
     df_export = pd.concat(
         [df_export, df_export_translingual]).reset_index(drop=True)
+    
     df_mentions = pd.concat(
         [df_mentions, df_mentions_translingual]).reset_index(drop=True)
 
@@ -189,8 +182,8 @@ def read_zip_files(dict_files, folder_path):
 
 
 # ------------------------------------------------------------------------
-def request1(dict_df, date, folder_path):
-    '''Pre-processing csv files that enable us to easily respond to the 
+def request1(dict_df):
+    '''Processing csv files so it enables us to easily respond to the 
     first CQL request on Cassandra. The purpose of this function is to 
     simplify the data before copying into the Cassandra database.
 
@@ -199,77 +192,98 @@ def request1(dict_df, date, folder_path):
     Parameters
     ---------
     - dict_df: type: dict,
-        Contain the 3 zip file names and DataFrames: export, mentions & 
+        Contains the 3 zip file names and DataFrames: export, mentions & 
         gkg (already concatenate for both English and Translingual 
         articles)
-    - date: type: type: str
-        Date of the processing zip file
-    - folder_path: type: str
-        Folder where the zip files are uploaded
 
     Return
     -----
-    Writes a '.csv' file containing the data processed.
-    
+    - df_req1: type: DataFrame
+        Contains the data ready to be inserted into a table on cassandra
+
     '''
     df_export = dict_df['export']
     df_mentions = dict_df['mentions']
 
-    # --- Attributs selection
-    col_req1_export = ['GlobalEventID', 'DATEADDED', 'Day', 
-        'ActionGeo_CountryCode', 'NumArticles']
+    # --- Attributs selection for the request
+    col_req1_export = ['GlobalEventID', 'Day', 'ActionGeo_CountryCode',
+        'NumArticles']
     col_req1_mentions = ['GlobalEventID', 'MentionDocTranslationInfo']
 
     df_req1 = df_export[col_req1_export]
-    df_req1 = df_req1.merge(df_mentions[col_req1_mentions],
-        how='inner', on='GlobalEventID')
 
-    # Possible to change NAN into a str like 'UK' if needed
+    df_req1 = df_req1.merge(
+        df_mentions[col_req1_mentions],
+        how='inner',
+        on='GlobalEventID')
+
+    # --- Update MentionDocTranslationInfo
     df_req1['MentionDocTranslationInfo'] = df_req1[
         'MentionDocTranslationInfo'].apply(lambda x: 
             x.split(';')[0].split(':')[-1] if isinstance(x, str) 
             else 'eng')
-
-    # --- Write df in csv
-    #folder = '/'.join(folder_path.split('/')[:-2]) + '/'
-    date = date.replace('-', '').replace(':', '').replace(' ', '-')
-    df_req1.to_csv(folder_path+'data-request1-{}.csv'.format(date),
-        index=False)
+    
+    return df_req1
 
 
 # ------------------------------------------------------------------------
-def request2(dict_df, date, folder_path):
-    '''Pre-processing csv files that enable us to easily respond to the 
-    second CQL request on Cassandra. 
+def cassandra_insert_req1(keyspace, table, df_data):
+    '''Insert data, row by row, into a user defined table in Cassandra.
 
     Parameters
     ---------
-    - dict_df: type: dict,
-        Contain the 3 zip file names and DataFrames: export, mentions & 
-        gkg (already concatenate for both English and Translingual 
-        articles)
-    - date: type: type: str
-        Date of the processing zip file
-    - folder_path: type: str
-        Folder where the zip files are uploaded
+    - keyspace: type: str
+        Name of the existing keyspace
+    - table: type: str
+        Name of the existing table for the request 1
+    - df_data: type: DataFrame
+        Contains all the data, pre-process, to be inserted in a table
+
+    '''
+    # --- Cassandra cluster connection
+    #cluster = Cluster()
+    #session = cluster.connect(keyspace)
+
+    # --- Data insertion
+    insert = "INSERT INTO {} (globaleventid, day, ".format(table) +\
+            "actiongeocountrycode, numarticles, mentiondoctranslationinfo)" +\
+            " VALUES ("
+    
+    df_req = df_data.dropna()  # NaN raises pbm if its not deleted
+
+    df_req.insert(
+        loc=df_req.shape[1],
+        column='insert_query',
+        value=insert+df_req['GlobalEventID'].astype(str)+','+
+            df_req['Day'].astype(str)+",'"+
+            df_req['ActionGeo_CountryCode']+"',"+
+            df_req['NumArticles'].astype(str)+",'"+
+            df_req['MentionDocTranslationInfo']+"');")
+    
+    # --- Execution of the insertion query
+    #df_req['insert_query'].apply(lambda x: session.execute(x))
+
+
+# ------------------------------------------------------------------------
+def format_date(date):
+    """Modify the shape of the day from YYYYMMDDHHMMSS 
+    to YYYY-MM-DD HH:MM:SS.
+
+    Parameter
+    ---------
+    - date: type: str, format: YYYYMMDDHHMMSS
 
     Return
     -----
-    Writes a '.csv' file containing the data processed.
-    
-    '''
-    df_export = dict_df['export']
+    - modified_date_shape: type: str, format: YYYY-MM-DD HH:MM:SS
 
-    col_req2_export = ['GlobalEventID', 'DATEADDED', 'Day', 'MonthYear',
-        'Year', 'ActionGeo_CountryCode', 'NumMentions']
+    """
+    d = '-'.join([date[:4], date[4:6], date[6:8]])
+    h = ':'.join([date[8:10], date[10:12]])
 
-    df_req2 = df_export[col_req2_export]
+    modified_date_shape = ' '.join([d, h])
 
-    # --- Write df in csv
-    #folder = '/'.join(folder_path.split('/')[:-2]) + '/'
-    date = date.replace('-', '').replace(':', '').replace(' ', '-')
-    df_req2.to_csv(folder_path+'data-request2-{}.csv'.format(date),
-        index=False)
+    return modified_date_shape
 
 
 # ------------------------------------------------------------------------
@@ -277,90 +291,86 @@ def request2(dict_df, date, folder_path):
 # ------------------------------------------------------------------------
 initial_date = sys.argv[1]  # str
 final_date = sys.argv[2]  # str
-delete_zip_files = False  # bool
+
+req1 = True
+req2 = False
+req3 = False
+req4 = False
 
 if __name__ == '__main__':
+    print('*'*75)
+    print('GDELT PROJECT - INF728 NOSQL\n')
+
+    t_init = t.time()
+    d_init = format_date(initial_date)
+    d_final = format_date(final_date)
+
     # --------------------------------------------------------------------
     # --- Generation of zip-links between initial and final dates
     print('-'*75)
-    print('Generating zip files, please wait ...\n')
-    t_init = t.time()
-    df_zip_files = generate_zip_files(initial_date=initial_date, 
+    print('Generating all zip files between {} '.format(d_init) +\
+        'and {}, \nplease wait...\n'.format(d_final))
+    
+    dict_zip_files = generate_zip_files(initial_date=initial_date, 
         final_date=final_date)
 
+    print('Time: {:.2f} s'.format(t.time()-t_init))
     print('-'*22, ' ZIP FILES GENERATION SUCCEED ', '-'*21, '\n')
-    
-    # --- Store all the files in a dictionnary
-    dict_zip_files = {}
 
-    for date in df_zip_files.index:
-        dict_zip_files[str(date)] = {
-            'eng-export': df_zip_files.loc[date]['eng-export'],
-            'translingual-export': df_zip_files.loc[date]['translingual-export'],
-            'eng-mentions': df_zip_files.loc[date]['eng-mentions'],
-            'translingual-mentions': df_zip_files.loc[date]['translingual-mentions'],
-            'eng-gkg': df_zip_files.loc[date]['eng-gkg'],
-            'translingual-gkg': df_zip_files.loc[date]['translingual-gkg']
-        }
-
-    # --- Lopp over all dates
+    # --- Lopp over all dates between init and final
     for key_date in dict_zip_files.keys():
-        print('*'*75)
-        print('Processing: {} \n'.format(key_date))
-        folder_date = key_date.replace(':', '.').replace(' ', '-')
-        folder_path = path + folder_date + '/'
+        t_process_init = t.time()
 
-        # --------------------------------------------------------------------
-        # --- Download of all zip files
         print('-'*75)
-        print('Downloading zip files, please wait ...\n')
-        for file in dict_zip_files[key_date].values():
-            wget_cmd = ['wget', file, '-P', folder_path]
-            local_cmd(wget_cmd)
+        print('- FILE: {}\n'.format(key_date))
 
-        print('-'*23, ' ZIP FILES DOWNLOAD SUCCEED ', '-'*22, '\n')
-        
-        # --------------------------------------------------------------------
-        # --- Pre-processing of zip files for requests
-        print('-'*75)
-        print('Pre-processing zip files, please wait ...\n')
-        dict_df = read_zip_files(dict_zip_files[key_date],
-            folder_path)
+        # ----------------------------------------------------------------
+        # --- Processing zip files for requests
+        print('Processing zip files, please wait...')
 
+        dict_df = read_zip_files(dict_zip_files[key_date])
+
+        # ----------------------------------------------------------------
+        # --- Cassandra : Insert files into a user define table
         # --- Request 1
-        request1(dict_df, key_date, folder_path)
-        print('-'*20, ' PRE-PROCESSING REQUEST 1 SUCCEED ', '-'*19, '\n')
+        if req1:
+            print('Request1: Inserting values into Cassandra, ' +\
+                'please wait...')
+            
+            #cassandra_insert_req1(KEYSPACE='gdelt_alann',
+            #    TABLE='test1',
+            #    df1=request1(dict_df))
 
         # --- Request 2
-        request2(dict_df, key_date, folder_path)
-        print('-'*20, ' PRE-PROCESSING REQUEST 2 SUCCEED ', '-'*19, '\n')
-    
+        if req2:
+            print('Request2: Inserting values into Cassandra, ' +\
+                'please wait...')
+
         # --- Request 3
-
+        if req3:
+            print('Request3: Inserting values into Cassandra, ' +\
+                'please wait...')
+        
         # --- Request 4
+        if req4:
+            print('Request4: Inserting values into Cassandra, ' +\
+                'please wait...')
+        
+        print('\nTime: {:.2f} s'.format(t.time()-t_process_init))
+        print('-'*22, ' PROCESS & INSERTION SUCCEED ', '-'*22, '\n')
 
-        # --------------------------------------------------------------------
-        # --- Delete all zip files
-        print('-'*75)
-        print('Deleting zip files, please wait ...\n')
-        if os.path.exists(folder_path) & delete_zip_files:
-            rm_cmd = ['rm', '-r', folder_path]
-            local_cmd(rm_cmd)
+    # --- Display execution
+    exec_time = t.time()-t_init
+    h = exec_time // 3600
+    min = (exec_time - 3600*h) // 60
+    sec = (exec_time - 3600*h) % 60
 
-            print('-'*23, ' ZIP FILES DOWNLOAD SUCCEED ', '-'*22, '\n')
-        else :
-            print('No deletion of zip files\n')
+    if exec_time >= 3600:
+        print('Total Execution Time: ' +\
+            '{} h {} min {:.0f} s'.format(h, min, sec))
+    elif exec_time < 60:
+        print('Total Execution Time: {:.2f} s'.format(sec))
+    else:
+        print('Total Execution Time: {} min {:.0f} s'.format(min, sec))
 
-        # --------------------------------------------------------------------
-        # --- Docker : copy csv into a containeur 
-
-        # --------------------------------------------------------------------
-        # --- Cassandra : copy files into DB (queries with cassandra-driver)
-
-        # --------------------------------------------------------------------
-        # --- Delete all zip files
-
-    print('Estimated time : {:.2f}s'.format(t.time()-t_init))
-
-
-    
+    print('*'*27, ' EXECUTION SUCCEED ', '*'*27, '\n')
